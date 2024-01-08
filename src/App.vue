8062bab2465ca2d70eaf6cdcdfeef8193f14bde6
gds90 <you@example.com>
2024-01-08 18:22:25 +0100

    BONUS: added new component for the light blue section

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,23 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppBuyProduct from './components/AppBuyProduct.vue';
 
 export default {
   components: {
     AppHeader,
     AppMain,
-    AppFooter
+    AppFooter,
+    AppBuyProduct
   }
 }
 </script>
 <template lang="">
   <AppHeader />
   <AppMain />
+  <AppBuyProduct/>
   <AppFooter />
+
 </template>
 <style lang="scss">
 @use './styles/generals.scss'
---
+++ src/components/AppBuyProduct.vue
@@ -0,0 +1,91 @@
+<script>
+export default {
+    name: 'AppBuyProduct',
+    data() {
+        return {
+            sections: [
+                {
+                    label: 'DIGITAL COMICS',
+                    img: 'buy-comics-digital-comics.png'
+                },
+                {
+                    label: 'DC MERCHANANDISE',
+                    img: 'buy-comics-merchandise.png'
+                },
+                {
+                    label: 'SUBSCRIPTION',
+                    img: 'buy-comics-subscriptions.png'
+                },
+                {
+                    label: 'COMIC SHOP LOCATOR',
+                    img: 'buy-comics-shop-locator.png'
+                },
+                {
+                    label: 'DC POWER VISA',
+                    img: 'buy-dc-power-visa.svg'
+                },
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+
+}
+</script>
+<template lang="">
+    <section class="buyProduct">
+        <div class="contentBuyProduct">
+            <ul>
+                <li v-for="(section, index) in sections" :key="index">
+                    <a>
+                        <img :src="getImagePath(`../assets/img/${section.img}`)" alt="/">
+                        <span class="label">{{section.label}}</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+
+.buyProduct {
+    background: $brand_primary;
+    height: 150px;
+    position: relative;
+    z-index: 1;
+
+
+    ul {
+        display: flex;
+        list-style: none;
+        height: 100%;
+        justify-content: space-around;
+        align-items: center;
+
+        li a {
+            display: flex;
+            align-items: center;
+        }
+    }
+
+    img {
+        max-width: 50px;
+    }
+}
+
+.contentBuyProduct {
+    max-width: 1200px;
+    margin: 0 auto;
+    height: 100%;
+}
+
+.label {
+    color: white;
+    font-size: 12px;
+    margin: 15px;
+}
+</style>
