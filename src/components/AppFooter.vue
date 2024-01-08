31de09dbf163cdc2d1e8d9adce74f9d415eba87b
gds90 <you@example.com>
2024-01-08 16:25:24 +0100

    Starting work on footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,10 +4,36 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
+    <footer>
+        <div class="footerUp">
+            <div class="contentFooterUp">
+
+            </div>
+        </div>
+        <div class="footerDown">
+            <div class="contentFooterDown">
+
+            </div>
+        </div>
+    </footer>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+
+.footerUp {
+    height: 350px;
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.footerDown {
+    background-color: $secondary_color;
+    height: 100px;
+}
+
+.contentFooterUp,
+.contentFooterDown {
+    max-width: 1200px;
+}
 </style>
