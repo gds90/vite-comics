0d97717b82ca00e4d2dbabca37ff61ddb552c83e
gds90 <you@example.com>
2024-01-09 14:08:55 +0100

    Added link on top footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -25,6 +25,114 @@ export default {
                     img: 'footer-periscope.png',
                     url: '#',
                 }
+            ],
+            dc_comics_menu: [
+                {
+                    label: 'Characters',
+                    url: '#'
+                },
+                {
+                    label: 'Comics',
+                    url: '#'
+                },
+                {
+                    label: 'Movies',
+                    url: '#'
+                },
+                {
+                    label: 'TV',
+                    url: '#'
+                },
+                {
+                    label: 'Games',
+                    url: '#'
+                },
+                {
+                    label: 'Videos',
+                    url: '#'
+                },
+                {
+                    label: 'News',
+                    url: '#'
+                }
+            ],
+            shop_menu: [
+                {
+                    label: 'Shop DC',
+                    url: '#'
+                },
+                {
+                    label: 'Shop DC Collectibles',
+                    url: '#'
+                }
+            ],
+            dc_menu: [
+                {
+                    label: 'Terms of Use',
+                    url: '#'
+                },
+                {
+                    label: 'Privacy policy (New)',
+                    url: '#'
+                },
+                {
+                    label: 'Ad Choices',
+                    url: '#'
+                },
+                {
+                    label: 'Advertising',
+                    url: '#'
+                },
+                {
+                    label: 'Jobs',
+                    url: '#'
+                },
+                {
+                    label: 'Subscriptions',
+                    url: '#'
+                },
+                {
+                    label: 'Talent Workshops',
+                    url: '#'
+                },
+                {
+                    label: 'CPSC Certificates',
+                    url: '#'
+                },
+                {
+                    label: 'Ratings',
+                    url: '#'
+                },
+                {
+                    label: 'Shop Help',
+                    url: '#'
+                },
+                {
+                    label: 'Contact Us',
+                    url: '#'
+                }
+            ],
+            sites_menu: [
+                {
+                    label: 'DC',
+                    url: '#'
+                },
+                {
+                    label: 'MAD Magazine',
+                    url: '#'
+                },
+                {
+                    label: 'DC Kids',
+                    url: '#'
+                },
+                {
+                    label: 'DC Universe',
+                    url: '#'
+                },
+                {
+                    label: 'DC Power Visa',
+                    url: '#'
+                }
             ]
         }
     },
@@ -37,17 +145,48 @@ export default {
 </script>
 <template lang="">
     <footer>
+        <!-- Top del footer  -->
         <div class="footerUp">
             <div class="contentFooterUp">
                 <div class="footerLinks">
-                    QUI CI VANNO I LINK DA CREARE COL V-FOR
-                        
+                    <!-- Menù con i link nel footer con sfondo -->
+                    <div class="menu">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li v-for="(link, index) in dc_comics_menu" :key="index">
+                                <a :href="link.url">{{link.label}}</a>
+                            </li>
+                        </ul>
+                        <h3 id="shop-label">SHOP</h3>
+                        <ul>
+                            <li v-for="(link, index) in shop_menu" :key="index">
+                                <a :href="link.url">{{link.label}}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="menu">
+                        <h3>DC</h3>
+                        <ul>
+                            <li v-for="(link, index) in dc_menu" :key="index">
+                                <a :href="link.url">{{link.label}}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="menu">
+                        <h3>SITES</h3>
+                        <ul>
+                            <li v-for="(link, index) in sites_menu" :key="index">
+                                <a :href="link.url">{{link.label}}</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="footerLogo">
-                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                    <img src="../assets/img/dc-logo-bg.png" alt="DC Comics">
                 </div>
             </div>
         </div>
+        <!-- Bottom del footer  -->
         <div class="footerDown">
             <div class="contentFooterDown">
                 <div>
@@ -75,6 +214,35 @@ export default {
     background-image: url('../assets/img/footer-bg.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+
+    .footerLinks {
+        display: flex;
+
+        h3 {
+            color: white;
+        }
+
+        .menu {
+            margin: 0px 20px;
+
+            ul {
+                display: block;
+
+                li {
+                    font-size: 12px;
+                    margin: 6px 0;
+
+                    a {
+                        color: #b6b6b6;
+                    }
+                }
+            }
+
+            h3 {
+                margin: 20px 0;
+            }
+        }
+    }
 }
 
 .footerDown {
@@ -83,6 +251,14 @@ export default {
     position: relative;
     z-index: 99999;
 
+    button {
+        border: 2px solid $brand_primary;
+        background-color: transparent;
+        color: #fff;
+        padding: 0.8rem;
+        font-weight: 500;
+        font-size: 16px;
+    }
 }
 
 .contentFooterUp,
@@ -100,14 +276,6 @@ img {
     bottom: 20%;
 }
 
-button {
-    border: 2px solid $brand_primary;
-    background-color: transparent;
-    color: #fff;
-    padding: 0.8rem;
-    font-weight: 500;
-    font-size: 16px;
-}
 
 .followLinks {
     display: flex;
@@ -123,7 +291,6 @@ button {
 ul {
     display: flex;
     list-style: none;
-    height: 100%;
     align-items: center;
 
     li {
